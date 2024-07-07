c8fe98b116bbb1f0df64847f2ffd335f52239fad
emmanuelle <emmanuelledennemont@orange.fr>
2024-07-07 08:55:46 +0200

    Initial Commit

---
+++ ListEffect/ListEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListEffectApp.swift
+//  ListEffect
+//
+//  Created by Emmanuelle  Dennemont on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
